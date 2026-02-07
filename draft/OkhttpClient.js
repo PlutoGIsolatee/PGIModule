94d64c0ba84109284f87d6dcae9341e58ad32011
PlutoGIsolatee <plutoqweguo@126.com>
2026-02-07 11:02:02 +0800

    创建 draft/OkhttpClient.js

---
+++ draft/OkhttpClient.js
@@ -0,0 +1,17 @@
+function OkhttpClient({
+        followRedirects = true,
+        connectTimeout = 10000,
+        readTimeout = 30000,
+        writeTimeout = 3000
+    } = {}) {
+        const ms = Packages.java.util.concurrent.TimeUnit.MILLISECONDS;
+
+        this.javaOkhttpClient = new OkhttpClient.JavaOkhttpClient()
+            .followRedirects(followRedirects)
+            .connectTimeout(connectTimeout, ms)
+            .readTimeout(readTimeout, ms)
+            .writeTimeout(writeTimeout, ms)
+            .build();
+    }
+
+    OkhttpClient.JavaOkhttpClient = Packages.okhttp3.OkhttpClient;
