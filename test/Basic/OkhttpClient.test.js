a5a129bcdda2a6311f545027d84a412b4fa737b3
PlutoGIsolatee <plutoqweguo@126.com>
2026-02-07 16:01:38 +0800

    更新 OkhttpClient.test.js

--- test/Basic/OkhttpClient.test.js
+++ test/Basic/OkhttpClient.test.js
@@ -1,23 +1,65 @@
+@js:
 /**
  * OK
  */
 
-function execute() {
-    function OkhttpClient({
-        followRedirects = true,
-        connectTimeout = 10000,
-        readTimeout = 30000,
-        writeTimeout = 3000
-    } = {}) {
-        const ms = Packages.java.util.concurrent.TimeUnit.MILLISECONDS;
+const OKHTTP_CLIENT_INSTANCE = Symbol("OkHttpClient.instance");
 
-        this.javaOkhttpClient = new OkhttpClient.JavaOkhttpClient()
-            .followRedirects(followRedirects)
-            .connectTimeout(connectTimeout, ms)
-            .readTimeout(readTimeout, ms)
-            .writeTimeout(writeTimeout, ms)
-            .build();
+function OkHttpClient({
+    followRedirects = true,
+    retryOnConnectionFailure = true,
+    connectTimeout = 10,
+    readTimeout = 30,
+    writeTimeout = 30
+} = {}) {
+    const s = Packages.java.util.concurrent.TimeUnit.SECONDS;
+
+    let builder;
+
+    if (!this.OKHTTP_CLIENT_INSTANCE) {
+        builder = new OkHttpClient.JavaOkHttpClient.Builder();
+    } else {
+        builder = this.OKHTTP_CLIENT_INSTANCE
+            .newBuilder();
     }
 
-    OkhttpClient.JavaOkhttpClient = Packages.okhttp3.OkhttpClient;
+    this.javaOkHttpClient = builder
+        .followRedirects(followRedirects)
+        .retryOnConnectionFailure(retryOnConnectionFailure)
+        .connectTimeout(connectTimeout, s)
+        .readTimeout(readTimeout, s)
+        .writeTimeout(writeTimeout, s)
+        .build();
 }
+
+OkHttpClient.JavaOkHttpClient = Packages.okhttp3.OkHttpClient;
+
+
+
+
+function expect(result) {
+    return {
+        toBe: function(actual) {
+            if (result !== actual) {
+                throw new Error(`预期值${actual}，实际值${result}`);
+            }
+        }
+    };
+}
+
+function test(desc, fn) {
+    try {
+        fn();
+        java.log(`${desc} 测试通过`);
+    } catch (error) {
+        java.log(`${desc} 测试失败，${error}`);
+    }
+}
+
+test("测试创建", () => {
+    new OkHttpClient();
+});
+
+test("测试方法", () => {
+    (new OkHttpClient())
+});
