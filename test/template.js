69ee9af96f470b3ce4025d0e12b51ef3be0fc08a
PlutoGIsolatee <plutoqweguo@126.com>
2026-02-07 14:12:42 +0800

    更新 template.js

--- test/template.js
+++ test/template.js
@@ -1,32 +1,33 @@
+@js:
 /**
  * UNTESTED
  */
 
-function execute() {
-    function expect(result) {
-        return {
-            toBe: function(actual) {
-                if (result !== actual) {
-                    throw new Error(`预期值${actual}，实际值${result}`);
-                }
+
+
+function expect(result) {
+    return {
+        toBe: function(actual) {
+            if (result !== actual) {
+                throw new Error(`预期值${actual}，实际值${result}`);
             }
-        };
-    }
-
-    function test(desc, fn) {
-        try {
-            fn();
-            console.log(`${desc} 测试通过`);
-        } catch (error) {
-            console.log(`${desc} 测试失败，${error}`);
         }
-    }
+    };
+}
 
-    test("测试", () => {
+function test(desc, fn) {
+    try {
+        fn();
+        console.log(`${desc} 测试通过`);
+    } catch (error) {
+        console.log(`${desc} 测试失败，${error}`);
+    }
+}
+
+test("测试", () => {
             expect(
 
                 .toBe(
 
                 );
             });
-    }
