1d837b9eeffae8537d83610fcf96cb219c6512ef
PlutoGIsolatee <plutoqweguo@126.com>
2026-02-18 10:30:19 +0800

    更新 testkit.js
    toThrow

--- test/testkit.js
+++ test/testkit.js
@@ -1,22 +1,72 @@
 function expect(result) {
-    return {
-        toBe(actual) {
-            if (result !== actual) {
-                throw new Error(`预期值${actual}，实际值${result}`);
-            }
-        },
-        toContain(element) {
-            if (!result.includes(element)) {
-                throw new Error(`预期包含${element}，实际值${result}`);
-            }
-        },
-        toHas(key) {
-            if (!Object.hasOwn(result, key)) {
-                throw new Error(`预期包含${key}，实际值${result}，可枚举键${Object.keys(result)}`);
-            }
-        }
-    };
+    return new Asserts(result);
 }
+
+function Asserts(src) {
+    this.src = src;
+}
+
+Asserts.prototype.log = function log(java) {
+    java.log(this.src);
+    return this;
+};
+
+Asserts.prototype.toBe = function toBe(expected) {
+    const result = this.src;
+    if (result !== expected) {
+        throw new Error(`预期值${expected}，实际值${result}`);
+    }
+    return this;
+};
+
+Asserts.prototype.toContain = function toContain(element) {
+    const result = this.src;
+    if (!result.includes(element)) {
+        throw new Error(`预期包含${element}，实际值${result}`);
+    }
+    return this;
+};
+
+Asserts.prototype.toNotContain = function toNotContain(element) {
+    const result = this.src;
+    if (result.includes(element)) {
+        throw new Error(`预期不包含${element}，实际值${result}`);
+    }
+    return this;
+};
+
+Asserts.prototype.toHas = function toHas(key) {
+    const result = this.src;
+    if (!Object.hasOwn(result, key)) {
+        throw new Error(`预期包含${key}，实际值${result}，可枚举键${Object.keys(result)}`);
+    }
+    return this;
+};
+
+Asserts.prototype.toNotHas = function toNotHas(key) {
+    const result = this.src;
+    if (Object.hasOwn(result, key)) {
+        throw new Error(`预期不包含${key}，实际值${result}，可枚举键${Object.keys(result)}`);
+    }
+    return this;
+};
+
+Asserts.prototype.toThrow = function toThrow(name) {
+    const fun = this.src;
+    let err;
+    try {
+        fun();
+    } catch (e) {
+        err = e;
+    }
+    if (!err) {
+        throw new Error(`预期抛出${name} Error，实际未抛出错误`);
+    } else if (err.name !== name) {
+        throw new Error(`预期抛出${name} Error，实际抛出${err}`);
+    }
+    return this;
+};
+
 
 function test(desc, fn) {
     const {
